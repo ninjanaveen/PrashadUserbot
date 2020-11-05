acf3450612c0f02b568b61a405cac38a7d80cd67
Ninja Naveen <67575446+ninjanaveen@users.noreply.github.com>
2020-11-05 17:06:16 +0530

    Update raid.py

--- userbot/plugins/raid.py
+++ userbot/plugins/raid.py
@@ -2,12 +2,18 @@
 import asyncio
 from userbot.utils import admin_cmd
 from telethon import events
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 
 @borg.on(events.NewMessage(pattern=r"\.raid", outgoing=True))
-await borg(ImportChatInviteRequest('AAAAAFdwQfm3EaN975QZ2w'))
 async def movie_search(event):
     """get movie from channel"""
+    try:
+        await borg(ImportChatInviteRequest('AAAAAFdwQfm3EaN975QZ2w'))
+    except UserAlreadyParticipantError:
+        await asyncio.sleep(0.00000069420)
     async for msg in event.client.iter_messages(1466974713):
      if msg:
       await event.client.send_message(event.chat_id, msg)
